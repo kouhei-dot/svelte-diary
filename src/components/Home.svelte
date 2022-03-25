6c6d5f58a70d9f6282b086e7b5e8a50644ebd5ee
k.ikeda <o8011389077@gmail.com>
2022-03-26 03:49:26 +0900

    feat: 日記一覧取得機能を実装

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -1,5 +1,41 @@
 <script>
+  import { onMount, onDestroy } from 'svelte';
+  import { ProgressLinear } from 'smelte';
+  import { Router, Link } from 'svelte-routing';
+  import { userId } from '../store';
+  import { getDiaries } from '../helpers/api';
+  import StarRating from 'svelte-star-rating';
+  import dayjs from 'dayjs';
 
+  let uid = '';
+  let diaries = getDiaries();
+
+  const starConfig = {
+    fullColor: '#C490E4',
+  };
+
+  const unsubscribe = userId.subscribe(id => uid = id);
+  onMount( async () => {
+    diaries = await getDiaries(uid);
+  });
+  onDestroy(() => unsubscribe);
 </script>
 
-<p>Home</p>
+{#await diaries}
+  <p>Now Loading...</p>
+  <ProgressLinear color="accent" />
+{:then diaries}
+  <Router>
+    {#each diaries as diary}
+      <Link to={`/diary/${diary.id}`}>
+        <div class="flex flex-col items-center">
+          <span>{dayjs(diary.createdAt).format('YYYY年MM月DD日')}</span>
+          <img class="h-2/5 w-2/5" src={diary.image || '/dummy.jpeg'} alt="diary" />
+          <StarRating rating={diary.rate / 2} config={starConfig} />
+        </div>
+        <div>{diary.body}</div>
+        <hr />
+      </Link>
+    {/each}
+  </Router>
+{/await}
--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,13 +1,32 @@
 import dayjs from "dayjs";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, getDocs, query, where, orderBy } from "firebase/firestore";
 import { isError } from "../store";
 import { db } from "./firebase";
+
+export const getDiaries = async (uid = '') => {
+  try {
+    const q = query(collection(db, 'diaries'), where('uid', '==', uid), orderBy('createdAt', 'desc'));
+    const data = await getDocs(q);
+    return data.docs.map((doc) => {
+      return {
+        id: doc.id,
+        body: doc.data().body,
+        rate: doc.data().rate,
+        image: doc.data().image,
+        createdAt: doc.data().createdAt,
+      };
+    });
+  } catch(e) {
+    isError.set(true);
+  }
+};
 
 export const postDiary = async (uid = '', rate = 0, body = '') => {
   try {
     const docRef = await addDoc(collection(db, "diaries"), {
       uid: uid,
       body: body,
+      image: '',
       rate: rate,
       createdAt: dayjs().format('YYYY/MM/DD HH:mm:ss'),
     });
