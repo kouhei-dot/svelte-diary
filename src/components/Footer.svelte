9922c96cf07f33e06f5ca6a7675ecc176bc97cbf
k.ikeda <o8011389077@gmail.com>
2022-04-05 00:10:02 +0900

    feat: フッターの内容実装、メニュー周りの機能を修正
    ハンバーガーメニューの出入りを滑らかに修正
    ログイン状態に応じて、メニュー内容が変わるように修正

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,9 +1,21 @@
 <script>
+  import { Router, Link } from 'svelte-routing';
+  import { onDestroy } from 'svelte';
+  import { userId } from '../store';
 
+  let uid;
+  const unsubscribe = userId.subscribe((id) => uid = id);
+  onDestroy(() => unsubscribe);
 </script>
 
-<footer class="bg-secondary-500">
-  フッター
+<footer class="bg-secondary-500 flex items-center justify-around">
+  <Router>
+    <Link class="block mb-3" to="/">Home</Link>
+    <Link class="block mb-3" to="about">日記の効果とは？</Link>
+    {#if uid}
+      <Link class="block mb-3" to="create">日記を書く</Link>
+    {/if}
+  </Router>
 </footer>
 
 <style>
--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -1,6 +1,6 @@
 <script>
-  import { fly, scale } from 'svelte/transition';
-  import { quadOut } from 'svelte/easing';
+  import { scale, slide } from 'svelte/transition';
+  import { quadOut, quadInOut } from 'svelte/easing';
   import { Router, Link } from 'svelte-routing';
   import { userId } from '../store';
   import { googleSignIn, googleSignOut } from '../helpers/firebase';
@@ -17,19 +17,23 @@
 </script>
 
 {#if open}
-  <nav class="bg-primary-900" on:click="{() => open = false}">
+  <nav
+    transition:slide={{duration: 300, easing: quadInOut}}
+    class="bg-primary-900 text-white-500"
+    on:click="{() => open = false}"
+  >
     <Router>
-      <Link class="block" to="/">Home</Link>
-      <Link class="block" to="about">About</Link>
-      <Link class="block" to="create">Create</Link>
+      <Link class="block mb-3" to="/">Home</Link>
+      <Link class="block mb-3" to="about">日記の効果とは？</Link>
       {#if uid}
+        <Link class="block mb-3" to="create">日記を書く</Link>
         <Link class="block" to="#" on:click={googleSignOut}>ログアウト</Link>
       {:else}
         <Link class="block" to="#" on:click={googleSignIn}>ログイン</Link>
       {/if}
     </Router>
   </nav>
-  <hr transition:scale={{ duration: 750, easing: quadOut, opacity: 1 }} />
+  <hr transition:scale={{delay: 300, duration: 750, easing: quadOut, opacity: 1 }} />
 {/if}
 
 <style>
@@ -39,7 +43,6 @@
     letter-spacing: 0.15em;
     padding: 1em;
     padding-top: 0;
-    color: #eef;
   }
   a {
     cursor: pointer;
